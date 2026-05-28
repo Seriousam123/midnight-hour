983cccd53521a6a0c3b571ad11b79c517771f31c
Matko Orsanic <orsanic.matko86@gmail.com>
2026-05-28 11:47:13 +0200

    background image

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,22 @@
 export default function Hero() {
   return (
-    <section className="hero" style={{minHeight: '70vh'}}>
-      <div className="heroBg" />
-      <div className="heroGrid" />
-      <div className="heroSlash" />
-      <div className="heroSlash2" />
-      <div className="scanlines" />
+    <section className="hero">
+      <div style={{
+        position: 'absolute',
+        inset: 0,
+        backgroundImage: 'url(/media/1.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        filter: 'blur(3px) brightness(0.25)',
+        transform: 'scale(1.05)',
+        zIndex: 0,
+      }} />
+      <div className="heroGrid" style={{zIndex: 1}} />
+      <div className="heroSlash" style={{zIndex: 1}} />
+      <div className="heroSlash2" style={{zIndex: 1}} />
+      <div className="scanlines" style={{zIndex: 1}} />
 
-      <div className="heroContent">
+      <div className="heroContent" style={{zIndex: 2}}>
         <h1 className="heroTitle">
           MIDNIGHT<span> HOUR</span>
         </h1>
